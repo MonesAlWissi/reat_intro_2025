053e7ad07e3a5bbe1d7f9ac7debc678a22c54ccf
MonesAlWissi <alwissi.mones@gmail.com>
2024-12-28 17:47:01 +0300

    .

--- intro/src/App.js
+++ intro/src/App.js
@@ -1,23 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
+import Home from './componants/navBar';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Home/>
+     
+     <img src={logo}/>
+     <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/4/43/Ambersweet_oranges.jpg/1200px-Ambersweet_oranges.jpg"></img>
+     
     </div>
   );
 }
---
+++ intro/src/componants/navBar.js
@@ -0,0 +1,15 @@
+import '../style/nav.css'
+export default function Home (){
+    return(
+<>
+<div id="navGrid">
+<img id="logoO" src="https://upload.wikimedia.org/wikipedia/commons/thumb/c/c8/Orange_logo.svg/640px-Orange_logo.svg.png"></img>
+
+<h2>home</h2>
+<h1>serch</h1>
+<h1>like</h1>
+<h1>like</h1>
+</div>
+</>
+    )
+}
